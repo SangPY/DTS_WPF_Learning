c0bb6266842e1263ff443779657d217b8f770f69
SangDoThanh <sangdothanh95@gmail.com>
2024-10-20 22:07:13 +0700

    CheckBox thay đổi bằng Triggle, không cần xử lý bằng JS

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,41 +25,6 @@ namespace DTS_WPF_Learning
         public MainWindow()
         {
             InitializeComponent();
-
-            DriveInfo[] drives = DriveInfo.GetDrives();
-            foreach (DriveInfo driveInfo in drives)
-                trvStructure.Items.Add(CreateTreeItem(driveInfo));
-        }
-
-        public void TreeViewItem_Expanded(object sender, RoutedEventArgs e)
-        {
-            TreeViewItem item = e.Source as TreeViewItem;
-
-            if ((item.Items.Count == 1) && (item.Items[0] is string))
-            {
-                item.Items.Clear();
-
-                DirectoryInfo expanderDir = null;
-                if (item.Tag is DriveInfo)
-                    expanderDir = (item.Tag as DriveInfo).RootDirectory;
-                else
-                    expanderDir = (item.Tag as DirectoryInfo);
-                try
-                {
-                    foreach (DirectoryInfo subDir in expanderDir.GetDirectories())
-                        item.Items.Add(CreateTreeItem(subDir));
-                }
-                catch { }
-            }    
-        }
-
-        private TreeViewItem CreateTreeItem(object o)
-        {
-            TreeViewItem item = new TreeViewItem();
-            item.Header = o.ToString();
-            item.Tag = o;
-            item.Items.Add("Loading...");
-            return item;
         }
     }
 
