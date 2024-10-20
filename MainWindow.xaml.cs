d318433d3a7588a7ac1b995fcdf2559e347af284
SangDoThanh <sangdothanh95@gmail.com>
2024-10-20 20:42:11 +0700

    treeview load data from class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,14 +25,29 @@ namespace DTS_WPF_Learning
         {
             InitializeComponent();
 
-            MenuItem root = new MenuItem() { Title = "Menu" };
+            //MenuItem root = new MenuItem() { Title = "Menu" };
 
-            MenuItem childItem1 = new MenuItem() { Title = "Child item #1" };
-            childItem1.Items.Add(new MenuItem() { Title = "Child item #1.1" });
-            childItem1.Items.Add(new MenuItem() { Title = "Child item #1.2" });
-            root.Items.Add(childItem1);
-            root.Items.Add(new MenuItem() { Title = "Child item #2" });
-            trvMenu.Items.Add(root);
+            //MenuItem childItem1 = new MenuItem() { Title = "Child item #1" };
+            //childItem1.Items.Add(new MenuItem() { Title = "Child item #1.1" });
+            //childItem1.Items.Add(new MenuItem() { Title = "Child item #1.2" });
+            //root.Items.Add(childItem1);
+            //root.Items.Add(new MenuItem() { Title = "Child item #2" });
+            //trvMenu.Items.Add(root);
+
+            List<Family> families = new List<Family>();
+            Family family1 = new Family() { Name = "Do thanh sang" };
+            family1.Members.Add(new FamilyMember() { Name = "DTS", Age = 29 });
+            family1.Members.Add(new FamilyMember() { Name = "SangDT", Age = 28 });
+            family1.Members.Add(new FamilyMember() { Name = "S DX", Age = 31 });
+            families.Add(family1);
+
+            Family family2 = new Family() { Name = "Do thanh hong an" };
+            family2.Members.Add(new FamilyMember() { Name = "Hong an", Age = 1 });
+            family2.Members.Add(new FamilyMember() { Name = "May", Age = 2 });
+
+            families.Add(family2);
+
+            trvFamilies.ItemsSource = families;
         }
     }
 
@@ -46,5 +61,25 @@ namespace DTS_WPF_Learning
         public string Title { get; set; }
 
         public ObservableCollection<MenuItem> Items { get; set; }
+    }
+
+    public class Family
+    {
+        public Family()
+        {
+            this.Members = new ObservableCollection<FamilyMember>();
+        }
+
+        public string Name { get; set; }
+
+        public ObservableCollection<FamilyMember> Members { get; set; }
+    }
+
+
+    public class FamilyMember
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
     }
 }
