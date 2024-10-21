4bb5b95398cd0b716cb84af75967540f8ad9359a
SangDoThanh <sangdothanh95@gmail.com>
2024-10-21 15:59:34 +0700

    Command - Implementing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,35 +27,29 @@ namespace DTS_WPF_Learning
             InitializeComponent();
         }
 
-        //private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        //{
-        //    MessageBox.Show("The new command is invoked");
-        //}
+        private void ExiCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
 
-        //private void NewCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        //{
-        //    e.CanExecute = true;
-        //}
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
 
-        //private void CutCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        //{
-        //    e.CanExecute = (txtEditor != null) && (txtEditor.SelectionLength > 0);
-        //}
-
-        //private void CutCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        //{
-        //    txtEditor.Cut();
-        //}
-
-        //private void PasteCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        //{
-        //    e.CanExecute = Clipboard.ContainsText();
-        //}
-
-        //private void PasteCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        //{
-        //    txtEditor.Paste();
-        //}
+        public static class CustomCommands
+        {
+            public static readonly RoutedUICommand Exit = new RoutedUICommand
+                (
+                "Exit",
+                "Exit",
+                typeof(CustomCommands),
+                new InputGestureCollection()
+                    {
+                        new KeyGesture(Key.F4, ModifierKeys.Alt)
+                    }
+                );
+        }
     }
 
 }
