12cb8a86f50b269329fe12865c3a51f42cd3ccd4
sangdothanh95 <sangdothanh95@gmail.com>
2024-10-29 14:41:09 +0700

    transfer picture by button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,15 +20,38 @@ namespace DTS_WPF_Learning
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Danh sách các đường dẫn hình ảnh
+        private List<string> _imagePaths = new List<string>
+        {
+            "Images/sang1.jpg",
+            "Images/sang2.jpg",
+            "Images/sang3.jpg"
+            // Thêm các đường dẫn hình ảnh khác ở đây
+        };
+
+        private int _currentIndex = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            ShowImage(_currentIndex); // Hiển thị hình ảnh đầu tiên
         }
 
-        private void TextBlock_MouseEnter(object sender, MouseEventArgs e)
+        private void ShowImage(int index)
         {
-            TextBlock txlb = sender as TextBlock;
-            txlb.Text = "DTS";
+            if (index >= 0 && index < _imagePaths.Count)
+            {
+                // Tạo một đối tượng BitmapImage và gán cho Image.Source
+                BitmapImage bitmap = new BitmapImage(new Uri(_imagePaths[index], UriKind.RelativeOrAbsolute));
+                DisplayImage.Source = bitmap;
+            }
+        }
+
+        private void NextImage_Click(object sender, RoutedEventArgs e)
+        {
+            // Tăng chỉ số ảnh hiện tại
+            _currentIndex = (_currentIndex + 1) % _imagePaths.Count; // Quay vòng lại khi đến cuối danh sách
+            ShowImage(_currentIndex);
         }
     }
 }
